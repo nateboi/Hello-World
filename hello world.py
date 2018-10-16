5143157df3c6c26779f9e0117941600474cd04f2
nateboi <44163734+nateboi@users.noreply.github.com>
2018-10-16 14:25:37 +0100

    Update hello world.py

--- hello world.py
+++ hello world.py
@@ -1,12 +1,10 @@
 print("hello world")
 
-name = 'david'
+name = input("What is your name?:")
+
 print ("hello",name)
 
-age = float(input("what's your age: "))
+age = int(input("what's your age: "))
 year = int(input("What is the year:"))
 birth = year - age 
 print("You were born in",birth)
-
-
-
